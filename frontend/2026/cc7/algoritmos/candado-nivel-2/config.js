cf9741857ae9530fda8619fc490fd70cc8616a6b
Pablo Macon <pablomacon@gmail.com>
2026-06-10 23:15:03 -0300

    Añadimos candado nivel 2

---
+++ frontend/2026/cc7/algoritmos/candado-nivel-2/config.js
@@ -0,0 +1,10 @@
+window.CANDADO_CONFIG = {
+  juegoSlug: "candado-nivel-2",
+  apiBaseUrl: "https://backend-actividades-liceo.vercel.app/api",
+
+  cantidadDigitos: 4,
+  permitirRepetidos: false,
+
+  imagenCerrado: "../candado/assets/candado-4-base.png",
+  imagenAbierto: "../candado/assets/candado-4-abierto.png",
+};
